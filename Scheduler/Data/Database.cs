a1151fbad66af3534ccfec95347bdf2fda8c9fcd
Timothy Cebula <timothycebula@gmail.com>
2026-01-13 21:14:20 -0600

    DB connected

---
+++ Scheduler/Data/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.Data
+{
+    internal static class Database
+    {
+      private static string ConnectionString =>
+         ConfigurationManager.ConnectionStrings["client_schedule"].ConnectionString;
+
+      public static MySqlConnection GetOpenConnection()
+      {
+         var conn = new MySqlConnection(ConnectionString);
+         conn.Open();
+         return conn;
+      }
+   }
+}
--- Scheduler/LoginForm.cs
+++ Scheduler/LoginForm.cs
@@ -9,15 +9,17 @@ using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Scheduler.Data;
 
 namespace Scheduler
 {
-    public partial class LoginForm : Form
-    {
-        public LoginForm()
-        {
-            InitializeComponent();
-        }
+   public partial class LoginForm : Form
+   {
+      public LoginForm()
+      {
+         InitializeComponent();
+      }
 
       private void LoginForm_Load(object sender, EventArgs e)
       {
@@ -28,7 +30,7 @@ namespace Scheduler
 
       private void ApplyLocalization(CultureInfo culture)
       {
-         var rm = new ResourceManager("Scheduler.LoginForm", typeof(LoginForm).Assembly);
+         ResourceManager rm = new ResourceManager("Scheduler.LoginForm", typeof(LoginForm).Assembly);
 
          Text = rm.GetString("Title_Login", culture) ?? Text;
 
@@ -38,17 +40,54 @@ namespace Scheduler
          btnLogin.Text = rm.GetString("Button_Login", culture) ?? btnLogin.Text;
          btnExit.Text = rm.GetString("Button_Exit", culture) ?? btnExit.Text;
       }
-   
+
 
 
       private void btnLogin_Click(object sender, EventArgs e)
-        {
+      {
+         string user = txtboxUsername.Text.Trim();
+         string pass = txtboxPassword.Text;
 
-        }
+         const string sql = @"
+SELECT userId
+FROM user
+WHERE userName=@u AND password=@p AND active=1
+LIMIT 1;";
 
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-    }
+         using (MySqlConnection conn = Database.GetOpenConnection())
+         using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+         {
+            cmd.Parameters.AddWithValue("@u", user);
+            cmd.Parameters.AddWithValue("@p", pass);
+
+            var userId = cmd.ExecuteScalar();
+            if (userId != null)
+            {
+               MessageBox.Show("You are being signed in");
+               // TODO: open main form
+               return;
+            }
+         }
+
+         // login failed -> show localized error
+         //ShowInvalidCredentials();
+
+
+         ComponentResourceManager rm = new ComponentResourceManager(typeof(LoginForm));
+         CultureInfo culture = CultureInfo.CurrentUICulture;
+
+         MessageBox.Show(
+             rm.GetString("Error_InvalidCredentials", culture) ?? "Invalid username/password.",
+             Text,
+             MessageBoxButtons.OK,
+             MessageBoxIcon.Error
+         );
+
+      }
+
+      private void btnExit_Click(object sender, EventArgs e)
+      {
+         this.Close();
+      }
+   }
 }
